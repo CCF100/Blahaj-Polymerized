19ca6630f1c9995db4e02430cdc7000de210687c
imreallybadatnames <pizzagtav@gmail.com>
2025-09-07 18:29:21 +1100

    fiks

--- src/main/java/hibi/blahaj/mixin/PlayerEntityRendererMixin.java
+++ src/main/java/hibi/blahaj/mixin/PlayerEntityRendererMixin.java
@@ -4,6 +4,7 @@ import hibi.blahaj.block.*;
 import net.minecraft.client.network.*;
 import net.minecraft.client.render.entity.*;
 import net.minecraft.client.render.entity.model.*;
+import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
 import org.spongepowered.asm.mixin.*;
@@ -13,16 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.*;
 @Mixin(PlayerEntityRenderer.class)
 public class PlayerEntityRendererMixin {
 	@Inject(
-		method = "getArmPose(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/util/Arm;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;",
+		method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;",
 		at = @At("TAIL"),
 		cancellable = true
 	)
-	private static void blahaj$cuddle(AbstractClientPlayerEntity player, Arm arm, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
-		// CROSSBOW_CHARGE only renders properly in offhand, soooooo
-		ItemStack stack = player.getStackInHand(arm == Arm.LEFT ? Hand.OFF_HAND : Hand.MAIN_HAND);
-		if (stack.getItem() instanceof CuddlyItem) {
+	private static void blahaj$cuddle(PlayerEntity player, ItemStack stack, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
+		if (stack.getItem() instanceof CuddlyItem)
 			cir.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_CHARGE);
-			cir.cancel();
-		}
 	}
 }
